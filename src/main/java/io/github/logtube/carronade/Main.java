9fd940da4c9a26c4185f0000848e730014582ab1
Guo Y.K <hi@guoyk.net>
2020-08-14 16:28:23 +0800

    cleanup

--- src/main/java/io/github/logtube/carronade/Main.java
+++ src/main/java/io/github/logtube/carronade/Main.java
@@ -1,78 +1,38 @@
 package io.github.logtube.carronade;
 
-import com.jsoniter.JsonIterator;
 import net.guoyk.eswire.ElasticWire;
-import net.guoyk.eswire.ElasticWireCallback;
 import net.guoyk.eswire.ElasticWireOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    private static String extractDocumentProject(byte[] bytes) throws IOException {
-        JsonIterator iter = JsonIterator.parse(bytes);
-        for (String field = iter.readObject(); field != null; field = iter.readObject()) {
-            if ("project".equals(field)) {
-                return iter.readString();
-            } else {
-                iter.skip();
-            }
-        }
-        return null;
-    }
-
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        HashMap<String, Integer> counters = new HashMap<>();
+        String index = args[0];
+
+        Workspace workspace = new Workspace(Paths.get(System.getProperty("workspace"), index).toString());
+        LOGGER.info("使用工作空间: {}", workspace.getWorkspace());
+
+        String[] dataDirs = Arrays.stream(System.getProperty("es.dataDirs")
+                .split(","))
+                .map(String::trim)
+                .toArray(String[]::new);
+        LOGGER.info("本机 ElasticSearch 数据目录: {}", (Object) dataDirs);
+
         ElasticWireOptions options = new ElasticWireOptions();
-        options.setDataDirs(new String[]{
-                "/data01/data",
-                "/data02/data",
-                "/data03/data",
-                "/data04/data",
-                "/data05/data",
-                "/data06/data",
-                "/data07/data",
-                "/data08/data",
-        });
+        options.setDataDirs(dataDirs);
         ElasticWire elasticWire = new ElasticWire(options);
-        elasticWire.export(args[0], new ElasticWireCallback() {
-
-            private long progress = 0;
-
-            private boolean sampled = false;
-
-            @Override
-            public boolean handleDocumentSource(byte[] bytes, long id, long total) {
-                if (!this.sampled) {
-                    LOGGER.info("sample = {}", new String(bytes));
-                    this.sampled = true;
-                }
-                long newProgress = (long) ((double) id * 100 / (double) total);
-                if (newProgress != this.progress) {
-                    LOGGER.info("progress = {}%", newProgress);
-                    this.progress = newProgress;
-                }
-                try {
-                    String project = extractDocumentProject(bytes);
-                    if (project == null) {
-                        LOGGER.error("missing project field");
-                        return false;
-                    }
-                    counters.put(project, counters.getOrDefault(project, 0) + 1);
-                    return true;
-                } catch (IOException e) {
-                    LOGGER.error("failed to extract project", e);
-                    return false;
-                }
-            }
-        });
-        LOGGER.info("aggregations = {}", counters);
+        elasticWire.export(index, workspace);
+        LOGGER.info("完成导出: {}", workspace.getCounters());
+        elasticWire.close();
+        workspace.close();
     }
 
 }
---
+++ src/main/java/io/github/logtube/carronade/Workspace.java
@@ -0,0 +1,128 @@
+package io.github.logtube.carronade;
+
+import com.jsoniter.JsonIterator;
+import net.guoyk.eswire.ElasticWireCallback;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.zip.GZIPOutputStream;
+
+public class Workspace implements ElasticWireCallback, Closeable, AutoCloseable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Workspace.class);
+
+    private final String workspace;
+
+    private final HashMap<String, AtomicLong> counters = new HashMap<>();
+
+    private final HashMap<String, FileOutputStream> fileOutputStreams;
+
+    private final HashMap<String, GZIPOutputStream> gzipOutputStreams;
+
+    private boolean sampled = false;
+
+    private long progress = 0;
+
+    public Workspace(String workspace) throws IOException {
+        FileUtils.deleteDirectory(new File(workspace));
+        FileUtils.forceMkdir(new File(workspace));
+        this.workspace = workspace;
+        this.fileOutputStreams = new HashMap<>();
+        this.gzipOutputStreams = new HashMap<>();
+    }
+
+    public String getWorkspace() {
+        return this.workspace;
+    }
+
+    public HashMap<String, AtomicLong> getCounters() {
+        return counters;
+    }
+
+    private String extractDocumentProject(byte[] bytes) {
+        JsonIterator iter = JsonIterator.parse(bytes);
+        try {
+            for (String field = iter.readObject(); field != null; field = iter.readObject()) {
+                if ("project".equals(field)) {
+                    return iter.readString();
+                } else {
+                    iter.skip();
+                }
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean handleDocumentSource(byte[] bytes, long id, long total) {
+        // sample
+        if (!this.sampled) {
+            LOGGER.info("sample = {}", new String(bytes));
+            this.sampled = true;
+        }
+        // progress
+        long progress = (long) ((double) id * 100 / (double) total);
+        if (progress != this.progress) {
+            LOGGER.info("progress = {}%", progress);
+            this.progress = progress;
+        }
+        // project
+        String project = extractDocumentProject(bytes);
+        if (project == null) {
+            LOGGER.error("missing project field or invalid json: {}", new String(bytes));
+            return false;
+        }
+        // get outputstream
+        GZIPOutputStream gzipOutputStream = this.gzipOutputStreams.get(project);
+        if (gzipOutputStream == null) {
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(Paths.get(this.workspace, project + ".ndjson.gz").toFile());
+                gzipOutputStream = new GZIPOutputStream(fileOutputStream);
+                this.fileOutputStreams.put(project, fileOutputStream);
+                this.gzipOutputStreams.put(project, gzipOutputStream);
+            } catch (IOException e) {
+                LOGGER.error("failed to create gzip file", e);
+                return false;
+            }
+        }
+        // count
+        AtomicLong counter = this.counters.get(project);
+        if (counter == null) {
+            counter = new AtomicLong();
+            this.counters.put(project, counter);
+        }
+        counter.incrementAndGet();
+        // write document
+        try {
+            gzipOutputStream.write(bytes);
+            gzipOutputStream.write('\n');
+        } catch (IOException e) {
+            LOGGER.error("failed to write gzip file", e);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void close() throws IOException {
+        for (GZIPOutputStream value : this.gzipOutputStreams.values()) {
+            value.close();
+        }
+        for (FileOutputStream value : this.fileOutputStreams.values()) {
+            value.close();
+        }
+        this.gzipOutputStreams.clear();
+        this.fileOutputStreams.clear();
+        this.counters.clear();
+    }
+}
